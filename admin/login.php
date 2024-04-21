14381d3c36f3344107cd621e728f6aaf4cad4eff
jakariya0dev <jhalhadi@gmail.com>
2024-04-22 00:04:22 +0600

    login page added

---
+++ admin/login.php
@@ -0,0 +1,52 @@
+<?php include_once 'config.php'?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <title>Corona Admin</title>
+    <?php include './components/header-links.php' ?>
+  </head>
+  <body>
+    
+    <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="row w-100 m-0">
+          <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
+            <div class="card col-lg-3 mx-auto">
+              <div class="card-body px-5 py-5">
+                <h3 class="card-title text-center mb-4">Admin Login</h3>
+                <form>
+                  <div class="form-group">
+                    <label>Username or email *</label>
+                    <input type="text" class="form-control p_input">
+                  </div>
+                  <div class="form-group">
+                    <label>Password *</label>
+                    <input type="text" class="form-control p_input">
+                  </div>
+                  
+                  <div class="text-center mt-4">
+                    <button type="submit" class="w-100 btn btn-primary btn-block enter-btn">Login</button>
+                  </div>
+                  <hr>
+                  <p class="sign-up">
+                    Don't have an Account or Forgot Password? <br>
+                    <span>Contact: +880152146603 (Support)</span>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+          <!-- content-wrapper ends -->
+        </div>
+        <!-- row ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+
+    <?php include './components/footer-links.php'?>
+  </body>
+</html>
